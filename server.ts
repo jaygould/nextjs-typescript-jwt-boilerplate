7c1d4d7407a23b27af24ea925d61ca601acac443
Jay Gould <i9055378@googlemail.com>
2019-03-29 17:27:15 +0000

    added types to node init server for next

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
 require('dotenv').config();
-const express = require('express');
-
+import * as express from 'express';
 const bodyParser = require('body-parser');
 const next = require('next');
 
@@ -15,23 +14,23 @@ app
 		server.use(bodyParser.urlencoded({ extended: true }));
 		server.use(bodyParser.json());
 
-		server.get('/custom/:id', (req, res) => {
+		server.get('/custom/:id', (req: express.Request, res: express.Response) => {
 			const actualPage = '/custom';
 			const queryParams = { id: req.params.id };
 			app.render(req, res, actualPage, queryParams);
 		});
-		server.get('*', (req, res) => {
+		server.get('*', (req: express.Request, res: express.Response) => {
 			return handle(req, res);
 		});
 
-		server.listen(3000, err => {
+		server.listen(3000, (err: express.Errback) => {
 			if (err) {
 				throw err;
 			}
 			console.log('> Ready on http://localhost:3000');
 		});
 	})
-	.catch(ex => {
+	.catch((ex: any) => {
 		console.error(ex.stack);
 		process.exit(1);
 	});
