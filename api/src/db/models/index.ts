553ef13f52d1e006af564b4499d5539f14b1b9eb
Jay <i9055378@googlemail.com>
2020-07-21 22:48:30 +0100

    Updated all packages and formatting on package filew

--- api/src/db/models/index.ts
+++ api/src/db/models/index.ts
@@ -24,7 +24,7 @@ fs.readdirSync(__dirname)
     );
   })
   .forEach((file: any) => {
-    const model = sequelize.import(path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize);
     db[model.name] = model;
   });
 
